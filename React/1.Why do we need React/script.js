96303e77f134d2126ee5e44579e612eee966f904
min <mohddanisha532@gmail.com>
2024-06-15 02:21:00 +0530

    dan150624

---
+++ React/1.Why do we need React/script.js
@@ -0,0 +1,8 @@
+let totalAplles=10;
+
+//document.getElementsByClassName("bsk1").
+let basket1Count=document.getElementsByClassName("bsk1")
+let left=document.querySelector('.left');
+console.log(left);
+console.log(basket1Count);
+basket1Count.innerText=totalAplles
